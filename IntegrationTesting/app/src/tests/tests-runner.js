af2f07ce1464adfb86d1c8993f834dcaca78730c
John Chynoweth <jchynowe1@yahoo.com>
2015-08-25 09:00:29 -0400

    add files

--- IntegrationTesting/app/src/tests/tests-runner.js
+++ IntegrationTesting/app/src/tests/tests-runner.js
@@ -7,10 +7,14 @@ export class TestsRunner
     Tests = [{id: 1, TestName: "test 1"}, {id: 2, TestName: "test 2"}, {id: 3, TestName: "test 3"}];
     constructor(testData) {        
         this.testData = testData;
+        this.TestToRun = "*";
     }
 
     activate() {
         
     }
     
+    showTests() {
+
+    }
 }
